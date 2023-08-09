bbc23898794ebc06aad37162fb730712c6b6fbd2
exit12dsz <141524235+exit12dsz@users.noreply.github.com>
2023-08-09 21:41:07 +0300

    Add files via upload

---
+++ we1/script.js
@@ -0,0 +1,35 @@
+const mobileToggle = document.getElementById("mobile-toggle");
+
+mobileToggle.addEventListener("click", () => {
+  document.querySelector(".nav-items").classList.toggle("active");
+});
+
+const navLinks = document.querySelectorAll(".nav-item").forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    document.querySelector(".nav-items").classList.toggle("active");
+  });
+});
+
+
+
+import translations from "./translations.js";
+
+const languageSelector = document.querySelector("select");
+languageSelector.addEventListener("change", (event) => {
+  setLanguage(event.target.value);
+  localStorage.setItem("lang", event.target.value);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const language = localStorage.getItem("lang") || "en"; // اذا لم تكن اللغة متوفرة استخدم الانجليزية
+  setLanguage(language);
+});
+
+const setLanguage = (language) => {
+  const elements = document.querySelectorAll("[data-i18n]");
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute("data-i18n");
+    element.textContent = translations[language][translationKey];
+  });
+  document.dir = language === "ar" ? "rtl" : "ltr";
+};
